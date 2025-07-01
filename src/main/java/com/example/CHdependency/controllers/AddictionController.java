6090425277eafbe605b875b24c3535aae025ae12
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 18:10:32 -0300

    feat: created route for creating an addiction

--- src/main/java/com/example/CHdependency/controllers/AddictionController.java
+++ src/main/java/com/example/CHdependency/controllers/AddictionController.java
@@ -6,4 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class AddictionController {
+    private final AddictionServices addictionServices;
+
+    AddictionController(AddictionServices addictionServices){
+        this.addictionServices = addictionServices;
+    }
+
+    @PostMapping("/addiction/create")
+    public ResponseEntity<String> createAddiction(@RequestBody AddictionDTO addiction){
+        boolean response = addictionServices.create(addiction);
+        if (!response) return ResponseEntity.status(400).body("something is wrong");
+        else return ResponseEntity.status(201).body("create a new addiction type successfully");
+    }
 }

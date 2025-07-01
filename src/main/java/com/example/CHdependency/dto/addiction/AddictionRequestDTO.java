0025c792007c83119df13e1cb490a1fbe0d47f18
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 18:15:50 -0300

    chore: created new dto to handle create addiction type

---
+++ src/main/java/com/example/CHdependency/dto/addiction/AddictionRequestDTO.java
@@ -0,0 +1,19 @@
+package com.example.CHdependency.dto.addiction;
+
+
+import com.example.CHdependency.enums.Addictions;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddictionRequestDTO {
+    @JsonProperty("type")
+    private Addictions type;
+}

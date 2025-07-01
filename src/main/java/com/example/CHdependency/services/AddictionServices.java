c28fbe6d2bcdd9c11c3e92d6a70569a60d923b26
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 18:12:04 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/services/AddictionServices.java
+++ src/main/java/com/example/CHdependency/services/AddictionServices.java
@@ -1,6 +1,8 @@
 package com.example.CHdependency.services;
 
 import com.example.CHdependency.repositories.AddictionRepository;
+import com.example.CHdependency.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service

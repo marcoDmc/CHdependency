9881e04d37d1351cc2b7b9d9fd9b2b757d22240a
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 08:07:08 -0300

    style: importing dependencies on meta repository

--- src/main/java/com/example/CHdependency/repositories/MetaRepository.java
+++ src/main/java/com/example/CHdependency/repositories/MetaRepository.java
@@ -1,6 +1,6 @@
 package com.example.CHdependency.repositories;
 
-import com.example.CHdependency.models.Meta;
+import com.example.CHdependency.entities.Meta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

8970d91502bc1bd50f02044b66be2d508c0e558b
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 14:03:12 -0300

    chore:  create a new DTO to user update password

---
+++ src/main/java/com/example/CHdependency/dto/UserPasswordDTO.java
@@ -0,0 +1,22 @@
+package com.example.CHdependency.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserPasswordDTO {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+    @JsonProperty("newPassword")
+    private String newPassword;
+
+}

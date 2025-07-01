2f30efd5b7138943a85e8b3e3ed28ccbf07a38e3
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 18:12:09 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/services/AddictionServices.java
+++ src/main/java/com/example/CHdependency/services/AddictionServices.java
@@ -1,5 +1,9 @@
 package com.example.CHdependency.services;
 
+import com.example.CHdependency.configuration.Config;
+import com.example.CHdependency.dto.addiction.AddictionDTO;
+import com.example.CHdependency.entities.Addiction;
+import com.example.CHdependency.entities.User;
 import com.example.CHdependency.repositories.AddictionRepository;
 import com.example.CHdependency.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;

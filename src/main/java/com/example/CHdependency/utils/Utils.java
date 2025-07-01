c24ab8bf3860692a70309c31ce452c15b3641635
marcoDmc <marcodamascenodev@gmail.com>
2025-07-01 08:21:00 -0300

    chore: update .gitignore and add new config and utils packages

---
+++ src/main/java/com/example/CHdependency/utils/Utils.java
@@ -0,0 +1,18 @@
+package com.example.CHdependency.utils;
+
+public class Utils {
+    public boolean validatePassword(String password){
+        String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#^+=])[A-Za-z\\d@$!%*?&#^+=]{8,}$";
+        return password.matches(passwordRegex);
+    }
+
+    public boolean validateEmail(String email){
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        return email.matches(emailRegex);
+    }
+
+    public boolean validateName(String name){
+        String nameRegex = "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$";
+        return name.matches(nameRegex);
+    }
+}

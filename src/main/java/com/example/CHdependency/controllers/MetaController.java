89e2c3721873652eea590b83f9d5c2d63783bbfb
marcoDmc <marcodamascenodev@gmail.com>
2025-07-02 17:27:20 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/controllers/MetaController.java
+++ src/main/java/com/example/CHdependency/controllers/MetaController.java
@@ -3,10 +3,9 @@ package com.example.CHdependency.controllers;
 import com.example.CHdependency.dto.meta.MetaDTO;
 import com.example.CHdependency.services.MetaServices;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")

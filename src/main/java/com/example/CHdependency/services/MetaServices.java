966e444b5ad206d94c7c3b9733b9e00a1ae875b4
marcoDmc <marcodamascenodev@gmail.com>
2025-07-02 16:25:32 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/services/MetaServices.java
+++ src/main/java/com/example/CHdependency/services/MetaServices.java
@@ -5,6 +5,9 @@ import com.example.CHdependency.dto.meta.MetaDTO;
 import com.example.CHdependency.entities.Metas;
 import com.example.CHdependency.entities.User;
 import com.example.CHdependency.repositories.MetaRepository;
+import com.example.CHdependency.repositories.UserRepository;
+import com.example.CHdependency.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service

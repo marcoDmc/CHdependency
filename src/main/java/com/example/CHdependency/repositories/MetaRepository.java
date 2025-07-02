3fccabb1ed6bbff93136d2fbad9aa466cbe9773d
marcoDmc <marcodamascenodev@gmail.com>
2025-07-02 16:31:42 -0300

    chore: meta repository created

--- src/main/java/com/example/CHdependency/repositories/MetaRepository.java
+++ src/main/java/com/example/CHdependency/repositories/MetaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MetaRepository extends JpaRepository<Meta, Long> {
+public interface MetaRepository extends JpaRepository<Metas, Long> {
 }

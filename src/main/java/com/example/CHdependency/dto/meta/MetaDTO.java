73a838bf48f338528c7fa7a6946572dcc3b053db
marcoDmc <marcodamascenodev@gmail.com>
2025-07-02 09:37:38 -0300

    chore: create a new dto to handle created meta

---
+++ src/main/java/com/example/CHdependency/dto/meta/MetaDTO.java
@@ -0,0 +1,19 @@
+package com.example.CHdependency.dto.meta;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MetaDTO {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("time")
+    private String time;
+}

040c20d0d2128545fb26ed613fd38dac8fdddb9c
marcoDmc <marcodamascenodev@gmail.com>
2025-07-02 16:28:02 -0300

    feat: route to create a new goal created

--- src/main/java/com/example/CHdependency/controllers/MetaController.java
+++ src/main/java/com/example/CHdependency/controllers/MetaController.java
@@ -11,4 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class MetaController {
+
+    private final MetaServices metaServices;
+
+
+    MetaController(MetaServices metaServices){this.metaServices = metaServices;}
+
+
+    @PostMapping("/meta/create")
+    public ResponseEntity<String> createMeta(@RequestBody MetaDTO meta){
+        boolean response  = metaServices.create(meta);
+        if (!response) return ResponseEntity.status(400).body("something is wrong");
+        else return ResponseEntity.status(201).body("create meta successfully");
+    }
 }

9e4aefdf62d2d70018c2276b5e04a0bbf7415270
marcoDmc <marcodamascenodev@gmail.com>
2025-07-03 17:35:54 -0300

    chore: created to delete an addiction

---
+++ src/main/java/com/example/CHdependency/dto/addiction/DeleteAddictionDTO.java
@@ -0,0 +1,21 @@
+package com.example.CHdependency.dto.addiction;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteAddictionDTO {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+    @JsonProperty("type")
+    private String type;
+}

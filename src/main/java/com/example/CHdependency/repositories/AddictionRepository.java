574165d10ddda29ccbd4746161602bf82df2d59b
marcoDmc <marcodamascenodev@gmail.com>
2025-07-03 17:29:33 -0300

    chore: create method in repository addiction to search for an addiction via foreign key

--- src/main/java/com/example/CHdependency/repositories/AddictionRepository.java
+++ src/main/java/com/example/CHdependency/repositories/AddictionRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddictionRepository extends JpaRepository<Addiction, Long> {
+    Addiction findByUserId(Long id);
 }

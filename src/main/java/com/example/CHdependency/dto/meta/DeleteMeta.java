892f21060845244b37a40a1974e202a0e8cd297f
marcoDmc <marcodamascenodev@gmail.com>
2025-07-03 17:30:06 -0300

    chore: created dto for deletion of a goal

---
+++ src/main/java/com/example/CHdependency/dto/meta/DeleteMeta.java
@@ -0,0 +1,21 @@
+package com.example.CHdependency.dto.meta;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeleteMeta {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
+    @JsonProperty("name")
+    private String name;
+}

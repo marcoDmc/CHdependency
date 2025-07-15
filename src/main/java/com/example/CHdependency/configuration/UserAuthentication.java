8ea581e600ba1ce111d33ff2c96faeb881be9c3e
marcoDmc <marcodamascenodev@gmail.com>
2025-07-14 23:39:00 -0300

    chore: Add UserAuthentication class for UserDetails implementation

--- src/main/java/com/example/CHdependency/configuration/UserAuthentication.java
+++ src/main/java/com/example/CHdependency/configuration/UserAuthentication.java
@@ -1,4 +1,52 @@
 package com.example.CHdependency.configuration;
 
-public class UserAuthentication {
+import com.example.CHdependency.entities.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserAuthentication implements UserDetails {
+
+    private final User user;
+
+    UserAuthentication(User user){
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "read");
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }

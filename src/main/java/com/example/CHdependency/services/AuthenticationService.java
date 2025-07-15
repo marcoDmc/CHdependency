eaac503c1144a6b4850000b6af7ea329d3d9969e
marcoDmc <marcodamascenodev@gmail.com>
2025-07-14 23:36:47 -0300

    chore: Add AuthenticationService for handling authentication logic

---
+++ src/main/java/com/example/CHdependency/services/AuthenticationService.java
@@ -0,0 +1,19 @@
+package com.example.CHdependency.services;
+
+import com.example.CHdependency.configuration.JwtService;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final JwtService jwtService;
+
+    AuthenticationService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String authenticate(Authentication authentication)
+    {
+        return jwtService.generateToken(authentication);
+    }
+}

f8f1960d11c368e843bae41b4b12dc2a61f044ac
marcoDmc <marcodamascenodev@gmail.com>
2025-07-15 17:16:38 -0300

    style: importing swagger dependencies

--- src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
+++ src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
@@ -22,6 +22,7 @@ import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 

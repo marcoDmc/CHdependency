3f52a95b5d1249459a90fdfeaa40c9406e67120b
marcoDmc <marcodamascenodev@gmail.com>
2025-07-14 23:35:52 -0300

    feat: plaintext
    Recolher
    
    Encapsular
    
    Copiar
    Add AuthenticationController for user authentication endpoint

---
+++ src/main/java/com/example/CHdependency/controllers/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.example.CHdependency.controllers;
+
+
+import com.example.CHdependency.dto.user.CrendentialsUserDTO;
+import com.example.CHdependency.services.AuthenticationService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationController(AuthenticationService authenticationService, AuthenticationManager authenticationManager) {
+        this.authenticationService = authenticationService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    @PostMapping("/authenticate")
+    public String authenticate(@RequestBody CrendentialsUserDTO authentication)
+    {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authentication.getUsername(), authentication.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return authenticationService.authenticate(auth);
+    }
+}

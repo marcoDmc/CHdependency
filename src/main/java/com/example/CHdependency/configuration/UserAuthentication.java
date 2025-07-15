8470d39cb17d13bc8627975c550686f1b730fb7b
marcoDmc <marcodamascenodev@gmail.com>
2025-07-14 23:31:41 -0300

    chore: create a new DTO with the name CredentialsUserDTO to handle credentials of user

---
+++ src/main/java/com/example/CHdependency/configuration/UserAuthentication.java
@@ -0,0 +1,4 @@
+package com.example.CHdependency.configuration;
+
+public class UserAuthentication {
+}
---
+++ src/main/java/com/example/CHdependency/dto/user/CrendentialsUserDTO.java
@@ -0,0 +1,19 @@
+package com.example.CHdependency.dto.user;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CrendentialsUserDTO {
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("password")
+    private String password;
+}

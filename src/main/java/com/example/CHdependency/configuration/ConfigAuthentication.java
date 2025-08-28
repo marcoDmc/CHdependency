0b49fa775c71a9f4121dc964aa0d2ce345753e72
marcoDmc <marcodamascenodev@gmail.com>
2025-08-27 21:12:18 -0300

    style: importing necessary dependencies

--- src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
+++ src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
@@ -22,6 +22,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPrivateKey;

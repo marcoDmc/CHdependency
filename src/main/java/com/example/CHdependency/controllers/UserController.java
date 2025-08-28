f208024242885bac2956772105616bacec82e608
marcoDmc <marcodamascenodev@gmail.com>
2025-08-27 21:29:32 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/controllers/UserController.java
+++ src/main/java/com/example/CHdependency/controllers/UserController.java
@@ -14,6 +14,10 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 

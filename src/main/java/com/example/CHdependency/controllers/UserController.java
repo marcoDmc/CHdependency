5bcaf52a7592085ed922f896e25266af5556eba1
marcoDmc <marcodamascenodev@gmail.com>
2025-08-27 21:29:36 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/controllers/UserController.java
+++ src/main/java/com/example/CHdependency/controllers/UserController.java
@@ -20,6 +20,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map;
 
 @Controller
 @RestController

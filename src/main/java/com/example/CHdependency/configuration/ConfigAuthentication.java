f5b09833e073c006ab93e706e4ccc8381d7db549
marcoDmc <marcodamascenodev@gmail.com>
2025-08-27 21:12:11 -0300

    style: importing necessary dependencies

--- src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
+++ src/main/java/com/example/CHdependency/configuration/ConfigAuthentication.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;

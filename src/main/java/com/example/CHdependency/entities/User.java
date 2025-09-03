ac967783bc23fddf91c0c04c406536debd930a93
marcoDmc <marcodamascenodev@gmail.com>
2025-09-03 08:44:16 -0300

    style: importing dependencies

--- src/main/java/com/example/CHdependency/entities/User.java
+++ src/main/java/com/example/CHdependency/entities/User.java
@@ -4,16 +4,7 @@ import java.util.List;
 
 import com.example.CHdependency.enums.user.Gender;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

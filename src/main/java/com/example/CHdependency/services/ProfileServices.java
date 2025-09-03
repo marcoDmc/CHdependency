0f46eb377932a98b24e0af8b3e66244031f2df0b
marcoDmc <marcodamascenodev@gmail.com>
2025-09-03 08:50:28 -0300

    feat: create ProfileServices with findProfile method
    - Added ProfileServices class as a Spring Service.
    - Injected ProfileRepository via constructor.
    - Implemented findProfile method to fetch a Profile by ID.

---
+++ src/main/java/com/example/CHdependency/services/ProfileServices.java
@@ -0,0 +1,20 @@
+package com.example.CHdependency.services;
+
+import com.example.CHdependency.entities.Profile;
+import com.example.CHdependency.repositories.ProfileRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProfileServices {
+    private final ProfileRepository profileRepository;
+
+    ProfileServices(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    public Optional<Profile> findProfile(Long id){
+        return profileRepository.findById(id);
+    }
+}
